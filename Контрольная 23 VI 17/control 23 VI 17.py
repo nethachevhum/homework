9a44a9d2772400ddba36849fd48a92195ef37fa7
netkachevhum <netkachev.hum@gmail.com>
2017-06-23 13:45:38 +0300

    fasfsa

--- Контрольная 23 VI 17/control 23 VI 17.py
+++ Контрольная 23 VI 17/control 23 VI 17.py
@@ -7,7 +7,7 @@ def open_file(name):
     f = open(name,"r")
     txt = f.read()
     f.close()
-    return re.findall("<w>.*</w>", txt)
+    return re.findall("<w>.*</w>[.,;:?!]*", txt)
 
 def write_to_file(st,filename): #просто записывает строку в новый файл; NB, что filename должен быть с форматом файла
     f = open(filename,"w+",encoding = "utf-8")
@@ -23,7 +23,7 @@ def count_words_infile(): #первое задание
     write_to_file(st,"output_1.txt")
 
 
-def find_data():
+def find_data(): ##второе задание
     fd = [] ##файл-автор-дата создания
     for roots,dirs,files in os.walk("."):
         for file in files:
@@ -39,7 +39,10 @@ def find_data():
     write_to_file(st,"output_2.csv")
 
 
-def find_bigrams():
+
+
+def find_bigrams(): #эта функция находит все биграммы. Теперь нужно найти предложения, в которых они лежат
+    bigrams = []
     for roots,dirs,files in os.walk("."):
         for file in files:
             if file.endswith(".xhtml"):
@@ -47,14 +50,35 @@ def find_bigrams():
                 for i,word in enumerate(ar): #а здесь можно было обойтись и без enumerate
                     if re.search("<w><ana lex=\"(.+)\" gr=\"A.*gen",word):
                         if re.search("<w><ana lex=\"(.+)\" gr=\"S.*gen",ar[i+1]):
-                            print(re.search("ana>(.+)<",word).group(1)+" " +re.search("ana>(.+)<",ar[i+1]).group(1))
+                            bigram = re.search("ana>(.+)<",word).group(1)+" " +re.search("ana>(.+)<",ar[i+1]).group(1)
+                            bigrams.append(bigram)
 
+
+def cleanse(): #в дальнейшем план, в целом, такой:
+    text = ""
+    for roots,dirs,files in os.walk("."):
+        for file in files:
+            if file.endswith(".xhtml"):
+                ar = open_file(os.path.join(roots,file))
+                for word in ar:
+
+                    text = text + " " + "".join(list(re.findall("ana>(.+)<.*([.,;:?!])?",word)))
+        
+    write_to_file(text,"test_cleanse.txt")
+                                    
                         
 
 
 
+def main():
+    find_bigrams()
+    find_data()
+    count_words_infile()
+    
+main()
 
-find_bigrams()
+
+#find_bigrams()
 
 
 
